93112f50fab946d17de43533af6546d9b8a7b8f3
asWorks <x10@asWorks.de>
2018-09-27 00:14:32 +0200

    Erweitert Test

--- TestingDomainClasses/UnitTest1.cs
+++ TestingDomainClasses/UnitTest1.cs
@@ -17,28 +17,35 @@ namespace TestingDomainClasses
             using (var db = new InvoiceModel())
             {
 
-                var ar = new AusgangsRechnung(DateTime.Now, "Ar_qwertz111");
-                ar.Netto = 667.17M;
-                ar.Zuzahlung = 99.12M;
+                for (int i = 0; i <125 ; i++)
+                {
+                    decimal s = .001M * i;
+                    var ar = new AusgangsRechnung(DateTime.Now, "Ar_2611-"+i);
+                    ar.Netto = 667.17M * s;
+                    ar.Zuzahlung = 99.12M * s;
 
-                db.AusgangsRechnungen.Add(ar);
+                    db.AusgangsRechnungen.Add(ar);
 
 
 
-                var er = new EingangsRechnung(DateTime.Now, "New_qwertz111");
-                db.EingangsRechnungen.Add(er);
-               var r= db.SaveChanges();
+                    var er = new EingangsRechnung(DateTime.Now, "Er_1971_+i");
+                    
+                    db.EingangsRechnungen.Add(er);
+                    var r = db.SaveChanges();
 
-                //var query = db.EingangsRechnungen.Where(id => id.RechnungsId == 1).SingleOrDefault();
-                var br = new BuchungsReference(er, DateTime.Now, "Alles gut");
-                var res = er.Ausbuchen(br);
-                var br1 = new BuchungsReference(ar, DateTime.Now, "Hat geklappt");
-                var arRes = ar.Ausbuchen(br1);
+                    //var query = db.EingangsRechnungen.Where(id => id.RechnungsId == 1).SingleOrDefault();
+                    var br = new BuchungsReference(er, DateTime.Now, "Alles gut");
+                    var res = er.Ausbuchen(br);
+                    var br1 = new BuchungsReference(ar, DateTime.Now, "Hat geklappt");
+                    var arRes = ar.Ausbuchen(br1);
 
-                db.BuchungsReferenzen.Add(br);
-                db.BuchungsReferenzen.Add(br1);
+                    db.BuchungsReferenzen.Add(br);
+                    db.BuchungsReferenzen.Add(br1); 
+                }
+
+
                 db.SaveChanges();
-                Console.WriteLine(er.RechnungsNummer);
+               //onsole.WriteLine(er.RechnungsNummer);
 
             }
 
