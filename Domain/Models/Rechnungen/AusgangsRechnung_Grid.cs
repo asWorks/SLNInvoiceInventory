0516f4ae27347ee952a0af88a12dce3ccd61322f
asWorks <x10@asWorks.de>
2018-11-04 13:43:59 +0100

    AddedAusgangsrechnungen_Grid

---
+++ Domain/Models/Rechnungen/AusgangsRechnung_Grid.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.Rechnungen
+{
+    public class AusgangsRechnung_Grid:AusgangsRechnung
+    {
+        public AusgangsRechnung_Grid(DateTime datum, string rechnungsnummer):base(datum,rechnungsnummer)
+        {
+
+        }
+
+        public AusgangsRechnung_Grid():base()
+        {
+
+        }
+
+
+       
+   
+
+        public string SetRechnungsNummer
+        {
+            get { return this._RechnungsNummer; }
+            set
+            {
+                if (value != this._RechnungsNummer)
+                {
+                    this._RechnungsNummer = value;
+                   
+                }
+            }
+        }
+
+
+    }
+}
